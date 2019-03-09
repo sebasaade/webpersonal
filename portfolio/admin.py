4bcccbb78781ad53c6a86a3e95e4e0923d930cfd
sebasaade <sebasaade@gmail.com>
2019-03-09 16:16:38 -0300

    primer commit

---
+++ portfolio/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import project
+
+# Register your models here.
+class projectAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(project, projectAdmin)
